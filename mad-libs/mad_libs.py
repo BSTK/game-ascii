2632c1cbfbe867d28275770c64ce8007a4786ad7
Bruno Luz <bsilva.se@gmail.com>
2022-03-05 17:09:49 -0300

    main - Primeira versão de Mad Libs

---
+++ mad-libs/mad_libs.py
@@ -0,0 +1,12 @@
+"""
+Game: Mad Libs - Histórias Loucas
+"""
+
+verbo_A = input('Informe um verbo: ')
+verbo_B = input('Informe um verbo: ')
+adjetivo = input('Informe um adjetivo: ')
+substantivo_plural = input('Informe um substantivo (plural): ')
+
+historia_louca = \
+    f'Programar é muito { adjetivo }! Sempre me emociona porque me encanta { verbo_A } ' \
+    f'problemas. Aprendenda a { verbo_B } com freeCodeCamp e alcanse seus { substantivo_plural }'
